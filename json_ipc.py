68cb0f13f5befdd2de26f9cec929c0106c56d1ff
litepresence <finitestate@tutamail.com>
2021-03-23 18:16:19 -0400

    Update json_ipc.py

--- json_ipc.py
+++ json_ipc.py
@@ -1,31 +1,39 @@
-def json_ipc(doc="", text="", initialize=False, append=False):
-    """
-    JSON IPC
+"""
+JSON IPC
 
-    Concurrent Interprocess Communication via Read and Write JSON
+Concurrent Interprocess Communication via Read and Write JSON
 
-    features to mitigate race condition:
+features to mitigate race condition:
 
-        open file using with statement
-        explicit close() in with statement
-        finally close()
-        json formatting required
-        postscript clipping prevents misread due to overwrite without erase
-        read and write to the text pipe with a single definition
-        growing delay between attempts prevents cpu leak
-
-    to view your live streaming database, navigate to the pipe folder in the terminal:
-
-        tail -F your_json_ipc_database.txt
-
+    open file using with statement
+    explicit close() in with statement
+    finally close()
+    json formatting required
+    postscript clipping prevents misread due to overwrite without erase
+    read and write to the text pipe with a single definition
+    growing delay between attempts prevents cpu leak
+    
     :dependencies: os, traceback, json.loads, json.dumps
     :warn: incessant read/write concurrency may damage older spinning platter drives
     :warn: keeping a 3rd party file browser pointed to the pipe folder may consume RAM
+
+to view your live streaming database, navigate to the pipe folder in the terminal:
+
+    tail -F your_json_ipc_database.txt
+
+wtfpl2020 litepresence.com
+"""
+
+import os
+import traceback, 
+from json import loads as json_loads
+from json import dumps as json_dumps
+
+def json_ipc(doc="", text="", initialize=False, append=False):
+    """
     :param str(doc): name of file to read or write
     :param str(text): json dumped list or dict to write; if empty string: then read
     :return: python list or dictionary if reading, else None
-
-    wtfpl2020 litepresence.com
     """
     # initialize variables
     data = None
